37115d3ae997c57bc617424e68391a2325520b79
ArtofCJC <artofcjc@gmail.com>
2023-06-17 14:37:52 -0400

    new push

---
+++ index.js
@@ -0,0 +1,41 @@
+const myDog = {
+    // Only change code below this line
+  'name': 'a string',
+  'legs': 4,
+  'tails': 2,
+  'friends': ['sonic','knuckles']
+  
+    // Only change code above this line
+  };
+
+  // Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line
+
+  // Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj['an entree'];   // Change this line
+  const drinkValue = testObj['the drink'];    // Change this line
+  
+  // Setup
+const myArr = [2, 3, 4, 5, 6];
+var total = 0;
+
+for(let i = 0; i < myArr.length; i++) {
+total += myArr[i]
+console.log(myArr[i])
+}
+// Only change code below this line
